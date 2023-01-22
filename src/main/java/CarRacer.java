50a84bfdf9d7c31e77a8d5910d34bf0007ad8675
Marshall F <marshall@wecancodeit.org>
2023-01-22 08:25:38 -0500

    Change CarRacer to be an actual object containing the cars in a race!

--- src/main/java/CarRacer.java
+++ src/main/java/CarRacer.java
@@ -1,18 +1,31 @@
 public class CarRacer {
+    Racecar redCar;
+    Racecar blueCar;
+    Racecar greenCar;
+    Racecar yellowCar;
+
+    public CarRacer(Racecar redCar, Racecar blueCar, Racecar greenCar, Racecar yellowCar) {
+        this.redCar = redCar;
+        this.blueCar = blueCar;
+        this.greenCar = greenCar;
+        this.yellowCar = yellowCar;
+    }
+
     /*The results of this race are incorrect, the correct result is:
-        Red Car in 1st with 2272.5 feet travelled
-        Blue Car in 4th with 1890 feet travelled
-        Green Car in 2nd with 2191.5 feet travelled
-        Yellow Car in 3rd with 2163 feet travelled
-     */
+            Red Car in 1st with 2272.5 feet travelled
+            Blue Car in 4th with 1890 feet travelled
+            Green Car in 2nd with 2191.5 feet travelled
+            Yellow Car in 3rd with 2163 feet travelled
+         */
     public static void main(String[] args) {
         int maxCounter = 50;
         int turnFrequency = 10;
         int turnLocation = 5;
-        runRace(new Racecar("Red Car", 60f, 5f), new Racecar("Blue Car", 40f, 10f), new Racecar("Green Car", 70f, 4f), new Racecar("Yellow Car", 50f, 7f), maxCounter, turnFrequency, turnLocation);
+        CarRacer carRacer = new CarRacer(new Racecar("Red Car", 60f, 5f), new Racecar("Blue Car", 40f, 10f), new Racecar("Green Car", 70f, 4f), new Racecar("Yellow Car", 50f, 7f));
+        carRacer.runRace(maxCounter, turnFrequency, turnLocation);
     }
 
-    private static void runRace(Racecar redCar, Racecar blueCar, Racecar greenCar, Racecar yellowCar, int maxCounter, int turnFrequency, int turnLocation) {
+    private void runRace(int maxCounter, int turnFrequency, int turnLocation) {
         for (int counter = 0; counter < maxCounter; counter++) {
             if (counter % turnFrequency == turnLocation) {
                 System.out.println("The cars hit a turn!");
