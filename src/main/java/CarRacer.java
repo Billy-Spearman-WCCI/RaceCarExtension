1eb33569e9ec2e88c3d078fee40bea840134ad4e
Marshall F <marshall@wecancodeit.org>
2023-01-22 08:11:46 -0500

    Unnecessary global variable stink

--- src/main/java/CarRacer.java
+++ src/main/java/CarRacer.java
@@ -1,8 +1,4 @@
 public class CarRacer {
-    private static Racecar redCar;
-    private static Racecar blueCar;
-    private static Racecar greenCar;
-    private static Racecar yellowCar;
     /*The results of this race are incorrect, the correct result is:
         Red Car in 1st with 2272.5 feet travelled
         Blue Car in 4th with 1890 feet travelled
@@ -10,10 +6,10 @@ public class CarRacer {
         Yellow Car in 3rd with 2163 feet travelled
      */
     public static void main(String[] args) {
-        redCar = new Racecar("Red Car", 60f, 5f);
-        blueCar = new Racecar("Blue Car", 40f, 10f);
-        greenCar = new Racecar("Green Car", 70f, 4f);
-        yellowCar = new Racecar("Yellow Car", 50f, 7f);
+        Racecar redCar = new Racecar("Red Car", 60f, 5f);
+        Racecar blueCar = new Racecar("Blue Car", 40f, 10f);
+        Racecar greenCar = new Racecar("Green Car", 70f, 4f);
+        Racecar yellowCar = new Racecar("Yellow Car", 50f, 7f);
         for (int counter = 0; counter < 50; counter++) {
             if (counter % 10 == 5) {
                 System.out.println("The cars hit a turn!");
@@ -34,11 +30,11 @@ public class CarRacer {
             yellowCar.accelerate();
             yellowCar.move();
             System.out.println(yellowCar.toString());
-            analyzeLeader();
+            analyzeLeader(redCar, blueCar, greenCar, yellowCar);
         }
     }
 
-    public static void analyzeLeader() {
+    public static void analyzeLeader(Racecar redCar, Racecar blueCar, Racecar greenCar, Racecar yellowCar) {
         int[] currentPositions = Racecar.determinePositions(redCar, blueCar, greenCar, yellowCar);
         redCar.outputPosition(currentPositions[0]);
         blueCar.outputPosition(currentPositions[1]);
